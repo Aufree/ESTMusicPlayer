79e783fee3698a72131bb21969a314896a6e5d7c
Aufree <freedomlijinfa@gmail.com>
2015-12-15 22:51:13 +0800

    Fixes #1

--- Enesco/Handlers/MusicHandler.m
+++ Enesco/Handlers/MusicHandler.m
@@ -47,13 +47,18 @@
         [dict setObject:@(audioDurationSeconds) forKey:MPMediaItemPropertyPlaybackDuration];
         CGFloat playerAlbumWidth = (SCREEN_WIDTH - 16) * 2;
         UIImageView *playerAlbum = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, playerAlbumWidth, playerAlbumWidth)];
+        UIImage *placeholderImage = [UIImage imageNamed:@"music_lock_screen_placeholder"];
         NSURL *URL = [BaseHelper qiniuImageCenter:music.cover
                                         withWidth:[NSString stringWithFormat:@"%.f", playerAlbumWidth]
                                        withHeight:[NSString stringWithFormat:@"%.f", playerAlbumWidth]];
         [playerAlbum sd_setImageWithURL:URL
-                       placeholderImage:[UIImage imageNamed:@"music_lock_screen_placeholder"]
+                       placeholderImage:placeholderImage
                               completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType, NSURL *imageURL) {
-                                  MPMediaItemArtwork *artwork = [[MPMediaItemArtwork alloc] initWithImage:playerAlbum.image];
+                                  if (!image) {
+                                      image = [UIImage new];
+                                      image = placeholderImage;
+                                  }
+                                  MPMediaItemArtwork *artwork = [[MPMediaItemArtwork alloc] initWithImage:image];
                                   playerAlbum.contentMode = UIViewContentModeScaleAspectFill;
                                   [dict setObject:artwork forKey:MPMediaItemPropertyArtwork];
                                   [[MPNowPlayingInfoCenter defaultCenter] setNowPlayingInfo:dict];
